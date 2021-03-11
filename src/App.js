08105d277c6dc52c2b37c08d7e4cbfdffd32b656
KRASAVA <vladimirova.a-a@yandex.ru>
2021-03-12 00:54:35 +0400

    Delete unnecessary curly braces

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import bg2 from './assets/bg2.jpg';
 const App = () => {
   return (
     <>
-        <Header title={'Привет!'} descr={'Тест'}/>
-        <Layout title={'Страница 1'} descr={'Описание'} urlBg={bg1}/>
-        <Layout title={'Страница 2'} descr={'Описание'} colorBg={'red'}/>
-        <Layout title={'Страница 3'} descr={'Описание'} urlBg={bg2}/>
+        <Header title='Привет!' descr='Тест'/>
+        <Layout title='Страница 1' descr='Описание' urlBg={bg1}/>
+        <Layout title='Страница 2' descr='Описание' colorBg={'red'}/>
+        <Layout title='Страница 3' descr='Описание' urlBg={bg2}/>
         <Footer />
     </>
   );
